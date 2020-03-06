3b8330d74bf54aa0ab72967f54cd79d859f926a0
Ivan Konovalenko <konovalenko11@gmail.com>
2020-03-05 22:06:49 -0800

    25

--- code/section_03/ByteShortIntLong/src/com/lombo/java/masterclass/Main.java
+++ code/section_03/ByteShortIntLong/src/com/lombo/java/masterclass/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Integer
         int myValue = 10000;
 
         int myMinIntValue = Integer.MIN_VALUE;
@@ -16,5 +17,37 @@ public class Main {
 
         int myIntTest = myMaxIntValue + 100;
         System.out.println("My overflowed int value: " + myIntTest);
+
+        // Byte
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Byte MIN value: " + myMinByteValue);
+        System.out.println("Byte MAX value: " + myMaxByteValue);
+        System.out.println("Busted MAX Byte value (implicit): " + (myMaxByteValue + 10));
+        System.out.println("Busted MAX Byte value (explicit): " + (byte)(myMaxByteValue + 10));
+
+        // Short
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        System.out.println("Short MIN value: " + myMinShortValue);
+        System.out.println("Short MAX value: " + myMaxShortValue);
+        System.out.println("Busted MAX Short value (implicit): " + (myMaxShortValue + 10));
+        System.out.println("Busted MAX Short value (explicit): " + (short)(myMaxShortValue + 10));
+
+        // Long
+        long myLongValue = 100L;
+        
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+
+        System.out.println("Long MIN value: " + myMinLongValue);
+        System.out.println("Long MAX value: " + myMaxLongValue);
+        System.out.println("Busted MAX Long value (implicit): " + (myMaxLongValue + 10));
+        System.out.println("Busted MAX Long value (explicit): " + (long)(myMaxLongValue + 10));
+
+        long myBigLongValue = 2_147_483_648L;
+
     }
 }
