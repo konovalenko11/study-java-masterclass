2187b86fbe31c7f91bdeabde3542aa3ff28da599
Ivan Konovalenko <konovalenko11@gmail.com>
2020-08-09 16:42:47 -0700

    Float and Double - updates

--- code/section_03/FloatAndDouble/src/lombo/com/Main.java
+++ code/section_03/FloatAndDouble/src/lombo/com/Main.java
@@ -15,8 +15,30 @@ public class Main {
         System.out.printf("Double MIN: [%s];%nDouble MAX: [%s]%n", myMinDoubleValue, myMaxDoubleValue);
 
         // Initialization
-        int myIntValue = 5;
-        float myFloatValue = (float) 5.25;
-        double myDoubleValue = 5d;
+        // Note: Double is the default type with precision.
+        int myIntValue = 5 / 3;
+        float myFloatValue = (float) 5 / 3f;
+        double myDoubleValue = 5d / 3d;
+
+        System.out.println("myIntValue: [" + myIntValue + "]");
+        System.out.println("myFloatValue: [" + myFloatValue + "]");
+        System.out.println("myDoubleValue: [" + myDoubleValue + "]");
+
+        // Challenge: Convert given number of pounds to kilograms.
+        double pounds = 12.52;
+        double kilos = poundToKilo(pounds);
+
+        System.out.println("Pounds: [" + pounds + "] -> Kilos: [" + kilos + "].");
+
+        // Output
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_5674444_890d;
+
+        System.out.println("PI: [" + pi + "].");
+        System.out.println("Another Number: [" + anotherNumber + "].");
+    }
+
+    public static double poundToKilo(double pounds){
+        return pounds * 0.45359237;
     }
 }
