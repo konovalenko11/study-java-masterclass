93496bc12f4fe723a8269a98c1aec678e792e910
Ivan Konovalenko <konovalenko11@gmail.com>
2020-08-09 17:28:45 -0700

    Strings. Lecture 31

---
+++ code/section_03/PrimitiveDataTypesRecap/src/lombo/Main.java
@@ -0,0 +1,37 @@
+package lombo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // byte
+	    // short
+	    // int
+	    // long
+	    // float
+	    // double
+	    // boolean
+	    // char
+
+        String myString = "This is my string";
+        System.out.println("myString is equal to " + myString);
+
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+
+        myString = myString + " \u00A9 2019";
+        System.out.println("myString is equal to " + myString);
+
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println("numberString: [" + numberString + "].");
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("lastString: [" + lastString + "].");
+
+        double doubleNumber = 120.47d;
+        lastString = lastString + doubleNumber;
+        System.out.println("lastString: [" + lastString + "].");
+    }
+}
