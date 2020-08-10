7204ac16d719054f7aef11d8435950a1ae12fef6
Ivan Konovalenko <konovalenko11@gmail.com>
2020-08-09 17:03:20 -0700

    Char and Boolean. Lecture 30.

---
+++ code/section_03/CharAndBool/src/lombo/Main.java
@@ -0,0 +1,19 @@
+package lombo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Chars
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+
+        System.out.println("Char: [" + myChar + "];");
+        System.out.println("Unicode char: [" + myUnicodeChar + "];");
+        System.out.println("Unicode char 2: [\u00A9];");
+
+        // Booleans
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
