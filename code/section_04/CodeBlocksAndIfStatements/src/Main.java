a276f15e7005fdc4414d61d7af65040d21af9e6a
Ivan Konovalenko <konovalenko11@gmail.com>
2021-10-04 22:32:53 -0700

    lesson 46 (Completed)

--- code/section_04/CodeBlocksAndIfStatements/src/Main.java
+++ code/section_04/CodeBlocksAndIfStatements/src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+        int score = 10000;
+        int levelCompleted = 8;
+        int bonus = 200;
 
-        if (score < 5000 && score > 1000) {
+        if (score < 20000 && score > 1000) {
             System.out.println("Your score was less than 5000 but greater than 1000");
         } else if (score < 1000) {
             System.out.println("Your score was less than 1000");
