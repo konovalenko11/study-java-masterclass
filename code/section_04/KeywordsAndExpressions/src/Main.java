92118bbbb38c9a5804f2af19e5e8b04f174e16f7
Ivan Konovalenko <konovalenko11@gmail.com>
2021-10-04 22:25:39 -0700

    lesson 46 (in progress)

---
+++ code/section_04/KeywordsAndExpressions/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // a mile is 1.609344 kilometres
+        double kilometres = (100 * 1.609344);
+
+        System.out.println("Kilometres: " + kilometres);
+    }
+}
---
+++ code/section_04/StatementsWhitespaceAndIndentation/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+
+        System.out.println("This is" +
+                " another statement.");
+    }
+}
