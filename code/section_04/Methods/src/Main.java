4916410385c852fd1b24d1eb53a21c27ffd0d778
Ivan Konovalenko <konovalenko11@gmail.com>
2021-10-07 21:28:00 -0700

    lesson 49

--- code/section_04/Methods/src/Main.java
+++ code/section_04/Methods/src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        calculateScore(true, 10000, 8, 200);
+        int highScore = calculateScore(true, 10000, 8, 200);
+        System.out.println("Your final score was " + highScore);
+
         calculateScore(false, 800, 8, 200);
+        System.out.println("Your final score was " + highScore);
+
+        // Challenge score.
+        displayHighScorePosition("Peter", calculateHighScorePosition(1500));
+        displayHighScorePosition("Jack", calculateHighScorePosition(900));
+        displayHighScorePosition("Valentin", calculateHighScorePosition(400));
+        displayHighScorePosition("Synthia", calculateHighScorePosition(50));
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -11,8 +20,22 @@ public class Main {
             finalScore = score + (levelCompleted * bonus);
         }
 
-        System.out.println("Your final score was " + finalScore);
-
         return finalScore;
+    }
+
+    public static void displayHighScorePosition (String name, int position) {
+        System.out.println(name + " managed to get into position " + position + " on the high score table.");
+    }
+
+    public static int calculateHighScorePosition (int score) {
+        if (score >= 1000) {
+            return 1;
+        } else if (score >= 500) {
+            return 2;
+        } else if (score >= 100) {
+            return 3;
+        } else {
+            return 4;
+        }
     }
 }
