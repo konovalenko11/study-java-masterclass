0082806d2be505e1820101a97fcf5fbf960d1754
Ivan Konovalenko <konovalenko11@gmail.com>
2021-10-24 17:10:37 -0700

    lesson 72 (completed)

--- code/section_05/ControlFlowStatements/src/ReadingUserInput.java
+++ code/section_05/ControlFlowStatements/src/ReadingUserInput.java
@@ -1,12 +1,23 @@
+import java.time.Year;
 import java.util.Scanner;
 
 public class ReadingUserInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your year of birth: ");
+        int yearOfBirth = scanner.nextInt();
+
+        // This will prevent the exit after nextInt.
+        scanner.nextLine();
+
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();
 
+        int age = Year.now().getValue() - yearOfBirth;
+
         System.out.println("Your name is " + name);
+        System.out.println("Your age is " + age);
 
         scanner.close();
     }
