faf58a1996823bb7bb69c38f9885ca5521696b87
Ivan Konovalenko <konovalenko11@gmail.com>
2021-10-24 19:25:33 -0700

    lecture 73 (completed)

--- code/section_05/ControlFlowStatements/src/ReadingUserInput.java
+++ code/section_05/ControlFlowStatements/src/ReadingUserInput.java
@@ -5,11 +5,17 @@ public class ReadingUserInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
+        int yearOfBirth = 1900;
 
-        // This will prevent the exit after nextInt.
-        scanner.nextLine();
+        System.out.println("Enter your year of birth: ");
+        if (scanner.hasNextInt()) {
+            yearOfBirth = scanner.nextInt();
+            // This will prevent the exit after nextInt.
+            scanner.nextLine();
+        } else {
+            System.out.println("Unable to parse year of birth.");
+            System.exit(1);
+        }
 
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();
