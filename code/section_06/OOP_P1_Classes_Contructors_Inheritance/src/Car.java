cf034c5ffbae607f7e20c7dc0d0db55eaf5520d9
Ivan Konovalenko <konovalenko11@gmail.com>
2021-10-25 21:37:56 -0700

    lesson 78 (in progress)

---
+++ code/section_06/OOP_P1_Classes_Contructors_Inheritance/src/Car.java
@@ -0,0 +1,23 @@
+public class Car {
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String colour;
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public String getEngine() {
+        return this.engine;
+    }
+}
